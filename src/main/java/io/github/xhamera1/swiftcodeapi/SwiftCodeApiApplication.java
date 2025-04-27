63fff064a90794119c7a6fe901772e04af351864
xhamera1 <chamerapatryk@gmail.com>
2025-04-27 23:25:26 +0200

    Initial project structure and Docker configuration

---
+++ src/main/java/io/github/xhamera1/swiftcodeapi/SwiftCodeApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.xhamera1.swiftcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftCodeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/xhamera1/swiftcodeapi/SwiftCodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xhamera1.swiftcodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftCodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
