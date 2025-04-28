79fe0d10c5a5ec504dd22a995d1f32cb477f1120
xhamera1 <chamerapatryk@gmail.com>
2025-04-29 00:19:08 +0200

    feat: Add dependencies and csv data file

--- src/main/java/io/github/xhamera1/swiftcodeapi/SwiftCodeApiApplication.java
+++ src/main/java/io/github/xhamera1/swiftcodeapi/SwiftCodeApiApplication.java
@@ -2,6 +2,7 @@ package io.github.xhamera1.swiftcodeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class SwiftCodeApiApplication {
